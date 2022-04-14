5470b56bf683b881fdb6fe0daacc700b934cdc0e
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-04-14 13:39:23 +0200

    Safer crypto_box_seal implem

--- libsodiumjni/src/main/c/unix/libsodiumjni_internal_SodiumApi.c
+++ libsodiumjni/src/main/c/unix/libsodiumjni_internal_SodiumApi.c
@@ -13,12 +13,21 @@ JNIEXPORT jint JNICALL Java_libsodiumjni_internal_SodiumApi_sodium_1init
 JNIEXPORT jint JNICALL Java_libsodiumjni_internal_SodiumApi_crypto_1box_1seal
   (JNIEnv *env, jobject obj, jbyteArray cipher, jbyteArray msg, jlong msg_len, jbyteArray pub_key) {
 
+    int cipher_len = (*env)->GetArrayLength(env, cipher);
+
+    if (cipher_len < crypto_box_SEALBYTES + msg_len)
+        return -10;
+
     jbyte *msg_bytes = (*env)->GetByteArrayElements(env, msg, 0);
     jbyte *pub_key_bytes = (*env)->GetByteArrayElements(env, pub_key, 0);
-    int cipher_len = (*env)->GetArrayLength(env, cipher);
     jbyte *cipher_bytes = malloc(cipher_len);
+
     int res = crypto_box_seal(cipher_bytes, msg_bytes, msg_len, pub_key_bytes);
-    (*env)->SetByteArrayRegion(env, cipher, 0, cipher_len, cipher_bytes);
+
+    if (res == 0) {
+        (*env)->SetByteArrayRegion(env, cipher, 0, cipher_len, cipher_bytes);
+    }
+
     free(cipher_bytes);
     (*env)->ReleaseByteArrayElements(env, pub_key, pub_key_bytes, JNI_ABORT);
     (*env)->ReleaseByteArrayElements(env, msg, msg_bytes, JNI_ABORT);
